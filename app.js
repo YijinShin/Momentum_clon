d7a3254ba36ce570dd3fd6044720e5815cb915ff
Yijin <49178058+o0heama0o@users.noreply.github.com>
2022-01-05 21:11:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const a = 5;
-const b = 4;
+const age = parseInt(prompt("How old are you?"));
+// age에 숫자가 아닌 다른것을 입력하면 parseInt 결과가 NaN이 나옴. 
+if (isNaN(age)){ 
+    console.log("Please write number");
+    
+}else if (age < 18 || ){
+    console.log("You are too young.")
+}
+else{
+    console.log("You can drink");
+}
 
-console.log(a+b);
-console.log(a+b);
+
