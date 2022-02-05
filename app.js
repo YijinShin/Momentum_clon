0a8b9fb9a9e1503c106f30ec75a0652eab22ac98
Yijin <49178058+o0heama0o@users.noreply.github.com>
2022-02-05 20:08:23 +0900

    바닐라js_ 3.7,3.8강

--- app.js
+++ app.js
@@ -1,38 +1,16 @@
-const h1 = document.querySelector(".hello h1"); 
+const h1 = document.querySelector("div.hello:first-child h1");  
 
-console.dir(h1); // title의 내부를 볼 수 있다. 
+
 
 function handleTitleClick(){
-    console.log("title was clicked");
-    h1.style.color = "red";
-}
-function handleTitleMouseenter(){
-    h1.innerText = "mouse is here";
-}
-function handleMouseLeave(){
-    h1.innerText = "mouse is leave";
-}
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy(){
-    alert("copied!");
-}
-function handleWindowOffline(){
-    alert("No wifi");
-}
-function handleWindowOnline(){
-    alert("No wifi");
+    //const clickedClass = "clicked";
+    //if(h1.classList.contains(clickedClass)) 
+    //    h1.classList.remove(clickedClass);
+    //else h1.classList.add(clickedClass); // css에 선언해놓은 active class를 불러옴
+    //위의 조건을 toggle로 줄일수있다. 
+    h1.classList.toggle("clicked"); // 리스트에 있으면 제거하고, 없으면 추가함 
 }
 
 h1.addEventListener("click", handleTitleClick);
 //위와 같은 이야기인데 이렇게도 쓸 수 있다. title.onclick = handleTitleClick;
 //그래도 addEventListener을 써서 리스너를 붙이는게 나중에 remove도 가능해서 좋음 
-h1.addEventListener("mouseenter", handleTitleMouseenter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-//window를 resize 
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy",handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
